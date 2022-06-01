c3a06a8e59223414e3341aaee6f851611ef25727
Milton Roberto Cuji Revelo <mrcuji@espe.edu.ec>
2022-05-31 19:25:16 -0500

    Examen
    examen primer parcial

--- Taller 2/js/main.js
+++ Taller 2/js/main.js
@@ -1,1 +1,13 @@
+$('[data-toggle="popover"]').popover();
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+// Accedemos al botón
+var emailInput = document.getElementById('floatingEmailInput');
+
+// evento para el input radio del "si"
+document.getElementById('verificar').addEventListener('click', function(e) {
+  console.log('Vamos a habilitar el input text');
+  emailInput.disabled = true;
+});
 
